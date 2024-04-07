140e0d45efe0f71c8d4c8d77491461cd9ee26470
Jaroslav Zabolotniy <yaroslavntu@ukr.net>
2024-04-07 16:42:25 +0300

    Add substraction operation

--- UnitTests.py
+++ UnitTests.py
@@ -9,6 +9,10 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(5, 3), 8)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(0, 0), 0)
-
+        
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(10, 4), 6)
+        self.assertEqual(self.calc.subtract(3, 5), -2)
+        self.assertEqual(self.calc.subtract(0, 0), 0)
 if __name__ == '__main__':
     unittest.main()
